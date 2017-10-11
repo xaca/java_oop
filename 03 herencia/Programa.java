999fb93839d093605eaeb4c3bf2391d565c05715
xaca <and1res@hotmail.com>
2017-10-11 09:03:41 -0500

    Se agrega una nueva clase heredada

--- 03 herencia/Programa.java
+++ 03 herencia/Programa.java
@@ -1,10 +1,15 @@
 public class Programa{
 	public static void main(String[] args) {
 		Animal a = new Elefante();
+		Animal zoo[] = {new Elefante(),new Lagartija()};
 		System.out.println(a.getNombre());
 		//a.crearCriasAleatorias(); Tenemos un error porque Animal no tiene este método
 		//System.out.println((Elefante)a.getNombre()); ¿Qué pasa con el casting explícito?
 		((Elefante)a).crearCriasAleatorias();
 		((Elefante)a).imprimirNombreCrias();
+
+		for(int i=0;i<zoo.length;i++){
+			System.out.println(zoo[i].getNombre());
+		}
 	}
 }
