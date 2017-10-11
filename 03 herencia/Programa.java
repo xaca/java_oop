ea087427071f226cad5efd8223cee83975df4e54
xaca <and1res@hotmail.com>
2017-10-11 08:51:27 -0500

    Se agrega un ejemplo del uso del casting explicito para cceder a miembros datos y metodos de una clase hija

--- 03 herencia/Programa.java
+++ 03 herencia/Programa.java
@@ -2,5 +2,9 @@ public class Programa{
 	public static void main(String[] args) {
 		Animal a = new Elefante();
 		System.out.println(a.getNombre());
+		//a.crearCriasAleatorias(); Tenemos un error porque Animal no tiene este método
+		//System.out.println((Elefante)a.getNombre()); ¿Qué pasa con el casting explícito?
+		((Elefante)a).crearCriasAleatorias();
+		((Elefante)a).imprimirNombreCrias();
 	}
 }
