16ca8a59edff2e3aa4d0cd146a077f784b3a29dc
jhugobb <hugo.b.brandao@gmail.com>
2017-10-19 20:10:38 +0100

    Add 5 characters

---
+++ gameofthrones/characters/Sansa.java
@@ -0,0 +1,9 @@
+public class Sansa {
+
+    private List<String> titles = new ArrayList<String>();
+    titles.add("Lady of Winterfell");
+	private String nickname = "Little Dove";
+	private String fullname = "Sansa Stark";
+	private String portrayedBy = "Sophie Turner";
+	private House house = "House Stark"
+}
