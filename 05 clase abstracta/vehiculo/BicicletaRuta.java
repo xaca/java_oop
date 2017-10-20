d51a494036d365dcf2aba8d75d17532203d5b32e
xaca <and1res@hotmail.com>
2017-10-20 09:15:48 -0500

    Se plantea una pregunta en la clase BiciletaDeRuta

--- 05 clase abstracta/vehiculo/BicicletaRuta.java
+++ 05 clase abstracta/vehiculo/BicicletaRuta.java
@@ -1,6 +1,9 @@
 class BicicletaRuta extends Bicicleta {
 	public float inclinacion;
 
+	/* Observe que el método realizarCambio es similar al cambiarCambio de la clase Carro,
+	   ¿Cómo podriamos estandarizar el nombre de los métodos?
+	*/
 	public void realizarCambio(int cambio_actual){
 		this.cambio_actual = cambio_actual;
 	}
