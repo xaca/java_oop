c399a4801216ab5c06eaa42e5cb94fe35a72b6b3
MrSkeletal <ian.kent91@gmail.com>
2017-10-20 19:14:13 -0500

    Added CerseiLannister
    Added 1 new Character

---
+++ gameofthrones/characters/CerseiLannister.java
@@ -0,0 +1,15 @@
+public class CerseiLannister {
+
+    private List<String> titles = new ArrayList<String>();
+    titles.add("Queen of the Andals and the First Men");
+    titles.add("Protector of the Seven Kingdoms");
+    titles.add("Lady of the Casterly Rock");
+    titles.add("Lady Paramount of the Westerlands");
+    titles.add("Wardeness of the West");
+    titles.add("Queen Mother");
+    titles.add("Queen Regent");
+	titles.add("Queen Consort");
+	private String fullname = "Queen Cersei I Lannister";
+	private String portrayedBy = "Lena Headey";
+	private House house = "House Lannister"
+}
