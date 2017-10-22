405106902832569bab88c3a6dec1611c901c787c
sherilpaulin <sherilpaulin96@gmail.com>
2017-10-22 11:51:07 -0400

    Updated tyrion lannister

--- gameofthrones/characters/TyrionLannister.java
+++ gameofthrones/characters/TyrionLannister.java
@@ -8,13 +8,14 @@
  *
  */
 public class TyrionLannister extends Character{
-	private List<String> titles = new ArrayList<String>();      
+	private List<String> titles;      
 	private String nickname;   
 	private String fullName;  
 	private String portrayedBy; 
 	private House house;   
 	
 	public TyrionLannister(House house) {
+		this.titles = new ArrayList<String>();
 		this.generateTitles();
 		this.nickname = "The Imp";
 		this.fullName = "Tyrion Lannister";
@@ -23,12 +24,13 @@ public class TyrionLannister extends Character{
 		this.house = house;
 	}
 
-	private List<String> generateTitles(){
+	private void generateTitles(){
 		this.titles.add("Lord of Casterly Rock");
 		this.titles.add("Lord Paramount of the Westerlands");
 		this.titles.add("Warden of the West");
 		this.titles.add("Hand of the Queen");
 		this.titles.add("Master of Coin");
 		this.titles.add("Acting Hand of the King");
+		
 	}
 }
