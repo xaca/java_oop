4a1ac687d3c7bfaf8ee87de5d6ca2380f4267c5f
xaca <xaca>
2017-10-22 20:31:57 -0500

    Se organiza un archivo con las practicas de abstraccion

--- practicas/TheWalkingDead.java
+++ practicas/TheWalkingDead.java
@@ -9,28 +9,30 @@ import java.util.List;
  *
  */
 public class location {
-	private Location hilltop;		  //Gregory
-	private Location kingdom;         //Ezekiel
-  private Location alexandria //Rick 
-  private Location sanctuary //Negan
+	private Location hilltop;//Gregory
+	private Location kingdom;//Ezekiel
+	private Location alexandria;//Rick 
+	private Location sanctuary;//Negan
 	private List<Location> maingroup; //Carl, Michonne Daryl,
 	
-	public location() {S}
+	public location(){
+
+	}
 
 	public Location getHilltop() {
 		return hilltop;
 	}
 
-  public Location getKingdom() {
+	public Location getKingdom() {
 		return kingdom;
 	}
-  public Location getAlexandria() {
+	public Location getAlexandria() {
 		return alexandria;
 	}
-  	public void setAlexandria(Location alexandria) {
+		public void setAlexandria(Location alexandria) {
 		this.alexandria = alexandria;
 	}
-  public Location getSanctuary() {
+	public Location getSanctuary() {
 		return sanctuary;
 	}
 
